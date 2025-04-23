fab47f0b74248cc2a3b274c0663620076d11b518
SrJhol <mendes.jhonatan039@gmail.com>
2025-04-22 21:25:08 -0300

    Javascript
    Criado o codigo, primeiramente não funcionando, mas já pré criado

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+exibirTabela(){
+const descInput = document.getElementById("descrição");
+const valorInput = document.getElementById("valor");
+const catInput = document.getElementById("categoria");
+const showButton = document.getElementById("show-button");
+const dadosExibidos = document.getElementById("dados");
+
+showButton.addEventListener("submit", () => {
+  const desc = descInput.value;
+  const valor = valorInput.value;
+  const categoria = catInput.value;
+
+  dadosExibidos.innerHTML = `<p>Descrição: ${desc}</p><p>Valor: ${valor}</p><p>Categoria: ${categoria}</p>`;
+});}
+
+Somar(){
+    let somando =+ valor
+}
